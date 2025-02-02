14f5d4256772fb5cc3c7386aa87d3c9a6dfeeca6
S.S.L. Tan-Ya <st712@cam.ac.uk>
2025-02-02 09:27:06 +0000

    SimplePin updates

---
+++ SimplePin/SimplePin_RR_output.m
@@ -0,0 +1,10 @@
+seed = -29501819;
+pop = 1750;
+Inactive_Cycles = 100;
+Active_Cycles = 500;
+Total_CPU_Time = 2.26599E+03;
+Total_Transport_Time = 1.13182E+02;
+Time_Per_Integration = 1.22237E-08;
+Clock_Time = 1.13208E+02;
+keff_keff = [ 1.23275E+00,5.15466E-05];
+
---
+++ SimplePin/tcCorrect_fluxlimited.py
@@ -0,0 +1,19 @@
+# TEMPLATE
+
+from viz_neutronics.transport_correction import generate_mg_XS
+
+
+
+
+
+MC_output_file = "SimplePin_MC_output.json"
+
+#tcType = 'outscatter'
+tcType = 'flux limited'
+
+
+generate_mg_XS(MC_output_file, tcType)
+
+
+
+# this will create a directory called materialsInputs with the materials XSfile inside.

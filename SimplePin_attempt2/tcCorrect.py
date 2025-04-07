265b296f21fbc65e2887bc7fd2331a6689ee15e2
S.S.L. Tan-Ya <st712@cam.ac.uk>
2025-04-07 16:36:20 +0100

    SimplePin attempt 2

---
+++ SimplePin_attempt2/tcCorrect.py
@@ -0,0 +1,27 @@
+# TEMPLATE
+
+from viz_neutronics.transport_correction import generate_mg_XS
+
+
+
+# MC_output_file = "SimplePin_MC_output.json"
+resultsFile= 'Results/'
+
+# filepathMC = resultsFile + "SimplePin_MC_output_100G.json"
+# filepathMC = "SimplePin_MC_output_500k.json"
+filepathMC = "SimplePin_MC_output_10^8.json"
+# filepathMC = "SimplePin_MC_output_70G_problematic.json"
+# filepathMC = 'SimplePinMC_output.json'
+
+
+
+# tcType = 'outscatter'
+tcType = 'flux limited'
+
+
+generate_mg_XS(filepathMC, tcType, switch='ON', plotting=True)
+# generate_mg_XS(filepathMC, tcType, switch='OFF', plotting=True)
+
+
+
+# this will create a directory called materialsInputs with the materials XSfile inside.

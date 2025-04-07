dd8a75dd6340e61d5d93c0e5be33b466233f1896
S.S.L. Tan-Ya <st712@cam.ac.uk>
2025-04-07 16:08:36 +0100

    Simple pin model added, with unstructured CASMO grids typed up and reference MC isotropic and anisotropic results

--- SimplePin/plotResults.py
+++ SimplePin/plotResults.py
@@ -1,20 +1,25 @@
 #from plottingFunctions import plotShannon, plotScatteringMatrices, plotFissionRatesMC, readInputs, readOutputs, plotFissionRatesRR
-from viz_neutronics.plottingFunctions import  plotShannon, plotScatteringMatrices, plotFissionRatesMC_radial, plotFluxMC, plotFissionRatesRR_radial, plotFissionRatesCompare_radial_MC_RR
+from viz_neutronics.plottingFunctions import  plotShannon, plotScatteringMatrices, plotFissionRatesMC_radial, plotFluxMC, plotFissionRatesMC, plotFissionRatesRR_radial, plotFissionRatesCompare_radial_MC_RR, plotFluxSpectrumMC, plotFissionRatesRR, plotFissionRatesCompareMC_RR
 
 inputFileMC = 'SimplePin_MC'
 outputFileMC = 'SimplePin_MC_output.json'
+# outputFileMC = 'SimplePin_MC_output_10^8.json'
 
 
 inputFileRR = 'SimplePin_RR'
 outputFileRR = 'SimplePin_RR_output.json'
 
 
+
 # plotScatteringMatrices(outputFileMC)
 # plotShannon(inputFileMC, outputFileMC)
-plotFissionRatesMC_radial(outputFileMC, normalise_plot=True, target=100)
+# plotFissionRatesMC_radial(outputFileMC, normalise_plot=True, target=100)
 # plotFluxMC(outputFileMC, target=100)
-plotFissionRatesRR_radial(outputFileRR, normalise_plot=True, target=100)
-
-plotFissionRatesCompare_radial_MC_RR(outputFileMC, outputFileRR)
+plotFissionRatesMC(outputFileMC, target=100)
+# plotFissionRatesRR(outputFileRR, target=100)
+# plotFissionRatesRR_radial(outputFileRR, normalise_plot=True, target=100)
+# plotFissionRatesCompareMC_RR(outputFileMC, outputFileRR,target = 100)
+# plotFissionRatesCompare_radial_MC_RR(outputFileMC, outputFileRR)
+plotFluxSpectrumMC(outputFileMC)
 
 
--- SimplePin/tcCorrect.py
+++ SimplePin/tcCorrect.py
@@ -7,7 +7,11 @@ from viz_neutronics.transport_correction import generate_mg_XS
 # MC_output_file = "SimplePin_MC_output.json"
 resultsFile= 'Results/'
 
-filepathMC = resultsFile + "SimplePin_MC_output_100G.json"
+# filepathMC = resultsFile + "SimplePin_MC_output_100G.json"
+# filepathMC = "SimplePin_MC_output_500k.json"
+# filepathMC = "SimplePin_MC_output_10^8.json"
+# filepathMC = "SimplePin_MC_output_70G_problematic.json"
+filepathMC = 'SimplePin_MC_output.json'
 
 
 
@@ -15,8 +19,8 @@ filepathMC = resultsFile + "SimplePin_MC_output_100G.json"
 tcType = 'flux limited'
 
 
-generate_mg_XS(filepathMC, tcType, switch='ON', plotting=True)
-#generate_mg_XS(filepathMC, tcType, switch='OFF', plotting=True)
+# generate_mg_XS(filepathMC, tcType, switch='ON', plotting=True)
+generate_mg_XS(filepathMC, tcType, switch='OFF', plotting=True)
 
 
 

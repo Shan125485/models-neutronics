7ac1da3734017a847938fdc7cc8b087cd3479b0c
S.S.L. Tan-Ya <st712@cam.ac.uk>
2025-05-06 17:12:13 +0100

    Quarter core model. Chaneg temperatures from 600K to 300K

---
+++ quarterCore/plotResults_MC.py
@@ -0,0 +1,30 @@
+#from plottingFunctions import plotShannon, plotScatteringMatrices, plotFissionRatesMC, readInputs, readOutputs, plotFissionRatesRR
+from viz_neutronics.plottingFunctions import  plotSpatialTallyMC, plotSpatialTallyRR, plotFissionRatesCompareMC_RR, plotFissionRatesRR, plotSpatialTallyCompare_MCRR, plotSpatialTallyCompare_MCMG, plotSpatialMaterialTallyMC
+
+inputFileMC = 'quarterCore_MC'
+outputFileMC = 'output.json'
+# outputFileMCMG = 'Colourset_MCMG_output.json'
+
+
+# inputFileRR = 'Colourset_RR'
+# outputFileRR = 'Colourset_RR_output.json'
+
+tallyName_MC = 'pinFiss'
+tallyName_MC_assem = 'assemblyFissRadial'
+
+# tallyName_MCMG = 'pinFiss'
+# tallyName_RR = 'fiss1G'
+
+
+# RR visualisation
+
+plotSpatialTallyMC(outputFileMC, tallyName_MC, normalise_by_mean='non-zero', response_index=1, visualise_quarter=False)
+plotSpatialTallyMC(outputFileMC, tallyName_MC_assem, normalise_by_mean='non-zero', response_index=1, visualise_quarter=False)
+# plotSpatialMaterialTallyMC(outputFileMC,'u238Capture', materialName='UO2-16' ,normalise_by_mean='all', response_index=0)
+# plotSpatialMaterialTallyMC(outputFileMC,'u238Capture', materialName='UO2-31' ,normalise_by_mean='all', response_index=0)
+
+# plotSpatialTallyRR(outputFileRR,tallyName_RR, visualise_quarter=False)
+# plotSpatialTallyCompare_MCRR(outputFileMC, outputFileRR, tallyName_MC, tallyName_RR, normalise_by_mean='all', response_index_MC=0, visualise_quarter=False)
+# plotSpatialTallyCompare_MCMG(outputFileMC,outputFileMCMG, tallyName_MC, tallyName_MCMG, normalise_by_mean='all', response_index_CE=0, response_index_MG=0, visualise_quarter=False)
+
+

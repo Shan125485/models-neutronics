504b6ba690e9e47cdadc7a455861a748bb3cd48b
S.S.L. Tan-Ya <st712@cam.ac.uk>
2025-05-15 15:25:07 +0100

    track tccorrect file

---
+++ quarterCore/tcCorrect.py
@@ -0,0 +1,26 @@
+# TEMPLATE
+
+from viz_neutronics.transport_correction import generate_mg_XS
+
+
+
+# MC_output_file = "SimplePin_MC_output.json"
+# resultsFile= 'Results/'
+
+# filepathMC = 'FuelAssembly_MC_output.json'
+
+filepathMC = 'quarterCore_MC_output_70G_aniso.json'
+
+
+
+
+# tcType = 'outscatter'
+tcType = 'flux limited'
+
+
+generate_mg_XS(filepathMC, tcType, switch='ON', plotting=True)
+# generate_mg_XS(filepathMC, tcType, switch='OFF', plotting=False)
+
+
+
+# this will create a directory called materialsInputs with the materials XSfile inside.

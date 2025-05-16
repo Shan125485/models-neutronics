e9f892c4d98f27d3af03767617767a75600f74f1
S.S.L. Tan-Ya <st712@cam.ac.uk>
2025-05-16 22:40:29 +0100

    Add quartCore model without steel reflector

--- quarterCore/plotResults_MC.py
+++ quarterCore/plotResults_MC.py
@@ -3,7 +3,7 @@ from viz_neutronics.plottingFunctions import  plotSpatialTallyMC, plotSpatialTal
 
 inputFileMC = 'quarterCore_MC'
 # outputFileMC = 'output.json'
-outputFileMC = 'quarterCore_MC_output_70G_aniso.json'
+outputFileMC = 'quarterCore_MC_output.json'
 # outputFileMCMG = 'Colourset_MCMG_output.json'
 
 
@@ -21,22 +21,23 @@ tallyName_RR2 = 'flux1G'
 
 # RR visualisation
 
-# plotSpatialTallyMC(outputFileMC, tallyName_MC, normalise_by_mean='non-zero', response_index=1, visualise_quarter='bottom-right')
-# plotSpatialTallyMC(outputFileMC, tallyName_MC_assem, normalise_by_mean='non-zero', response_index=1, visualise_quarter='bottom-right')
-plotSpatialTallyMC(outputFileMC, tallyName_MC, normalise_by_mean='non-zero', response_index=1, visualise_quarter=False)
-plotSpatialTallyMC(outputFileMC, tallyName_MC_assem, normalise_by_mean='non-zero', response_index=1, visualise_quarter=False)
+# plotSpatialTallyMC(outputFileMC, tallyName_MC, normalise_by_mean='non-zero', response_index=0, visualise_quarter='bottom-right')
+# plotSpatialTallyMC(outputFileMC, tallyName_MC_assem, normalise_by_mean='non-zero', response_index=0, visualise_quarter='bottom-right')
 
 plotSpatialTallyMC(outputFileMC, tallyName_MC, normalise_by_mean='non-zero', response_index=0, visualise_quarter=False)
 plotSpatialTallyMC(outputFileMC, tallyName_MC_assem, normalise_by_mean='non-zero', response_index=0, visualise_quarter=False)
+plotSpatialTallyMC(outputFileMC, tallyName_MC, normalise_by_mean='non-zero', response_index=1, visualise_quarter=False)
+plotSpatialTallyMC(outputFileMC, tallyName_MC_assem, normalise_by_mean='non-zero', response_index=1, visualise_quarter=False)
 
 
 
 # plotSpatialMaterialTallyMC(outputFileMC,'u238Capture', materialName='UO2-16' ,normalise_by_mean='all', response_index=0)
 # plotSpatialMaterialTallyMC(outputFileMC,'u238Capture', materialName='UO2-31' ,normalise_by_mean='all', response_index=0)
 
-plotSpatialTallyRR(outputFileRR,tallyName_RR, normalise_by_mean='non-zero', visualise_quarter=False)
-plotSpatialTallyRR(outputFileRR,tallyName_RR2, normalise_by_mean='non-zero', visualise_quarter=False)
-plotSpatialTallyCompare_MCRR(outputFileMC, outputFileRR, tallyName_MC, tallyName_RR, normalise_by_mean='non-zero', response_index_MC=0, visualise_quarter=False)
+# plotSpatialTallyRR(outputFileRR,tallyName_RR, normalise_by_mean='non-zero', visualise_quarter=False)
+# plotSpatialTallyRR(outputFileRR,tallyName_RR2, normalise_by_mean='non-zero', visualise_quarter=False)
+# plotSpatialTallyCompare_MCRR(outputFileMC, outputFileRR, tallyName_MC, tallyName_RR2, normalise_by_mean='non-zero', response_index_MC=0, visualise_quarter=False)
+# # plotSpatialTallyCompare_MCRR(outputFileMC, outputFileRR, tallyName_MC, tallyName_RR, normalise_by_mean='non-zero', response_index_MC=1, visualise_quarter=False)
 # plotSpatialTallyCompare_MCMG(outputFileMC,outputFileMCMG, tallyName_MC, tallyName_MCMG, normalise_by_mean='all', response_index_CE=0, response_index_MG=0, visualise_quarter=False)
 
 
